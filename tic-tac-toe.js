05e8a13aed4baef1a0f2bd174c9c84ed27a3c083
Rajahni Cunningham <rajahni.cunningham@mymona.uwi.edu>
2022-10-11 14:55:49 -0500

    Changes to the JS file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,8 @@
-let square = document.querySelectorAll("board div").classList.add(".square")
+window.onload = function() {
+    var board = document.getElementById("board").children;
+    console.log(board.length);
+
+    for (i=0; i <= board.length - 1; i++) {
+        board[i].className = "square";
+    }
+}
